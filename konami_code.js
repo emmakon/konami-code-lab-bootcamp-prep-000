4a8b4f524b973b8980b8261827212c5ac308f0af
Emma Konrad <emma.konrad92@gmail.com>
2017-09-23 14:54:39 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,27 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Key codes for A, B, and C keys.
-  const alphabet = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+  let body = document.body;
 
-  // Keep track of index outside of the event handler.
-  let index = 0;
-
-  // This is the function that would be invoked by the event listener.
-  function onKeyDownHandler(e) {
+  body.addEventListener('keypress', function(e) {
     const key = parseInt(e.detail || e.which);
 
-    if (key === alphabet[index]) {
-      index++;
+ if (key === alphabet[index]) {
+   index++;
 
-      if (index === alphabet.length) {
-        alert("Hurray!");
+   if (index === alphabet.length) {
+     alert("Hurray!");
 
-        index = 0;
-      }
-    } else {
-      index = 0;
-    }
+     index = 0;
+   }
+ } else {
+   index = 0;
+ }
+} );
   }
-
-}
